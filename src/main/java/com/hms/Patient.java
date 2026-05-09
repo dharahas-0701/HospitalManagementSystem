eda9496e7263a58fa0d282813611a479f74bd082
dharahas-0701 <radapudharahas@gmail.com>
2026-05-09 10:53:57 +0530

    Add Patient class with attributes and methods for patient management

---
+++ src/main/java/com/hms/Patient.java
@@ -0,0 +1,89 @@
+package com.hms;
+
+public class Patient {
+
+    public enum Gender {
+        MALE,
+        FEMALE,
+        OTHER
+    }
+
+    public enum BloodType {
+        A_POSITIVE,
+        A_NEGATIVE,
+        B_POSITIVE,
+        B_NEGATIVE,
+        AB_POSITIVE,
+        AB_NEGATIVE,
+        O_POSITIVE,
+        O_NEGATIVE
+    }
+
+    private int patientId;
+    private String name;
+    private int age;
+    private Gender gender;
+    private BloodType bloodGroup;
+    private String disease;
+    private String phone;
+
+    public Patient() {
+
+    }
+
+    public int getPatientId() {
+        return patientId;
+    }
+
+    public void setPatientId(int patientId) {
+        this.patientId = patientId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public void setGender(Gender gender) {
+        this.gender = gender;
+    }
+
+    public BloodType getBloodGroup() {
+        return bloodGroup;
+    }
+
+    public void setBloodGroup(BloodType bloodGroup) {
+        this.bloodGroup = bloodGroup;
+    }
+
+    public String getDisease() {
+        return disease;
+    }
+
+    public void setDisease(String disease) {
+        this.disease = disease;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
