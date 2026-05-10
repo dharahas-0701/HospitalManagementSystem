e7882e25e36b0d38bc8d6315a232cbb373c122be
dharahas-0701 <radapudharahas@gmail.com>
2026-05-10 16:42:40 +0530

    Enhance addPatient method to get patient ID and log success message

--- src/main/java/com/hms/PatientDAOImpl.java
+++ src/main/java/com/hms/PatientDAOImpl.java
@@ -9,7 +9,29 @@ public class PatientDAOImpl implements PatientDAO {
     Connection conn = DBConnection.getConnection();
     @Override
     public void addPatient(Patient p){
-
+        try{
+            String query = "Insert INTO patients" +
+                    "(name, age, gender,blood_group, disease, phone)" +
+                    "VALUES(?,?,?,?,?,?)";
+            PreparedStatement ps = conn.prepareStatement(query,
+                    PreparedStatement.RETURN_GENERATED_KEYS);
+            ps.setString(1, p.getName());
+            ps.setInt(2, p.getAge());
+            ps.setString(3, p.getGender().name());
+            ps.setString(4, p.getBloodGroup().name());
+            ps.setString(5, p.getDisease());
+            ps.setString(6, p.getPhone());
+            ps.executeUpdate();
+            ResultSet rs = ps.getGeneratedKeys();
+            if(rs.next()){
+                int id = rs.getInt(1);
+                p.setPatientId(id);
+                System.out.println("Patient added successfully");
+                System.out.println("The ID of the patient is : " + id);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
     @Override
     public ArrayList<Patient> getAllPatients(){
@@ -52,7 +74,7 @@ public class PatientDAOImpl implements PatientDAO {
     }
     @Override
     public void updatePatient(Patient p){
-        
+
     }
     @Override
     public void deletePatient(int id){
