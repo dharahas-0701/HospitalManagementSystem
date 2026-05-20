17182ab3fb533b50a8133e72118fd48f1144e112
dharahas-0701 <radapudharahas@gmail.com>
2026-05-20 15:24:50 +0530

    Enhance Bill class with additional properties and methods for comprehensive billing management

--- src/main/java/com/hms/billing/Bill.java
+++ src/main/java/com/hms/billing/Bill.java
@@ -11,5 +11,79 @@ public class Bill {
 
     private double consultationFee;
     private double medicineCharges;
-    private double
+    private double testCharges;
+    private double roomCharges;
+    private double totalAmount;
+
+    private PaymentStatus paymentStatus;
+
+    public Bill(){
+
+    }
+
+    public Bill(int patientId, int appointmentId, double consultationFee, double medicineCharges,
+                double testCharges, double roomCharges, PaymentStatus paymentStatus){
+        this.patientId = patientId;
+        this.appointmentId = appointmentId;
+        this.consultationFee = consultationFee;
+        this.medicineCharges = medicineCharges;
+        this.testCharges = testCharges;
+        this.roomCharges = roomCharges;
+        this.paymentStatus = paymentStatus;
+    }
+    public int getBillId() {
+        return billId;
+    }
+    public void setBillId(int billId) {
+        this.billId = billId;
+    }
+    public int getPatientId() {
+        return patientId;
+    }
+    public void setPatientId(int patientId) {
+        this.patientId = patientId;
+    }
+    public int getAppointmentId() {
+        return appointmentId;
+    }
+    public void setAppointmentId(int appointmentId) {
+        this.appointmentId = appointmentId;
+    }
+    public double getConsultationFee() {
+        return consultationFee;
+    }
+    public void setConsultationFee(double consultationFee) {
+        this.consultationFee = consultationFee;
+    }
+    public double getMedicineCharges() {
+        return medicineCharges;
+    }
+    public void setMedicineCharges(double medicineCharges) {
+        this.medicineCharges = medicineCharges;
+    }
+    public double getTestCharges() {
+        return testCharges;
+    }
+    public void setTestCharges(double testCharges) {
+        this.testCharges = testCharges;
+    }
+    public double getRoomCharges() {
+        return roomCharges;
+    }
+    public void setRoomCharges(double roomCharges) {
+        this.roomCharges = roomCharges;
+    }
+    public PaymentStatus getPaymentStatus() {
+        return paymentStatus;
+    }
+    public void setPaymentStatus(PaymentStatus paymentStatus) {
+        this.paymentStatus = paymentStatus;
+    }
+    public double getTotalAmount() {
+        return totalAmount;
+    }
+    public void setTotalAmount(double totalAmount) {
+        this.totalAmount = totalAmount;
+    }
+
 }
