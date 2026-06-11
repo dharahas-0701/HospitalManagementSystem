a0ac0e29eb22e37ec11e7d83ee5cf118afc09bf2
dharahas-0701 <radapudharahas@gmail.com>
2026-06-11 17:30:00 +0530

    Complete Patient Module CRUD functionality of Servlet based WebApp

--- src/main/java/com/hms/Main.java
+++ src/main/java/com/hms/Main.java
@@ -10,7 +10,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
+        Scanner sc  = new Scanner(System.in);
 
         while(true) {
             System.out.println("============== ABCD HOSPITAL ===============");
